bb58223efd7bd200f37b787cc2eb08de1cecff55
Nikita Chemeris <nikitacemeris9@gmail.com>
2023-03-28 23:04:13 +0300

    Changed Task_4, Lection_1

--- Lection_1/Task_4.py
+++ Lection_1/Task_4.py
@@ -1,12 +1,15 @@
 '''Quadratic equation'''
 
-a, b, c = [float(input()) for i in range(3)]
+a = float(input("Enter number 'a': "))
+b = float(input("Enter number 'b': "))
+c = float(input("Enter number 'c': "))
+
 D = b ** 2 - 4 * a * c  # Discriminant
 
 if a == 0:
-    print('Error')
+    print("'a' can't be zero")
 elif D < 0:
-    print('No real roots')
+    print('No real roots in Discriminant')
 else:
     print(f'x1 = {(-b + D ** 0.5) / (2 * a)}')
     print(f'x2 = {(-b - D ** 0.5) / (2 * a)}')
