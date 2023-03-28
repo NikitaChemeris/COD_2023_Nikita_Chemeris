80f0b80f4624216143c3120efe6611add5338c75
Nikita Chemeris <nikitacemeris9@gmail.com>
2023-03-28 23:53:53 +0300

    Changed Task_3, Lection_1

--- Lection_1/Task_3.py
+++ Lection_1/Task_3.py
@@ -7,18 +7,20 @@ operation = input('Enter operation: ')
 
 match operation:
     case '+':
-        print(f'Result: {number1 + number2}')
+        value = number1 + number2
     case '-':
-        print(f'Result: {number1 - number2}')
+        value = number1 - number2
     case '*':
-        print(f'Result: {number1 * number2}')
+        value = number1 * number2
     case '/':
-        print(f'Result: {number1 / number2}')
+        value = number1 / number2
     case '**':  # Find out the number in the degree
-        print(f'Result: {number1 ** number2}')
+        value = number1 ** number2
     case '//':  # Find the whole part
-        print(f'Result: {number1 // number2}')
+        value = number1 // number2
     case '%':  # Find remainder of a number
-        print(f'Result: {number1 % number2}')
+        value = number1 % number2
     case _:
-        print('Wrong Enter!')
+        value = 'Wrong Enter'
+
+print(f'Result: {value}')
