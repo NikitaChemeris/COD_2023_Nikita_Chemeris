67acfce30a759a9b20cb02489a6116b1c195abde
Nikita Chemeris <nikitacemeris9@gmail.com>
2023-03-30 23:29:59 +0300

    Added Task_3, Lection_2

---
+++ Lection_2/Task_3.py
@@ -0,0 +1,7 @@
+# 3 sets
+
+set1 = set(input('Enter elements for the first set, separated spaces: ').split())
+set2 = set(input('Now for the second set: ').split())
+set3 = set(input('And finally for the third set: ').split())
+
+print(f'Unique elements: {set1 ^ set2 ^ set3}')
