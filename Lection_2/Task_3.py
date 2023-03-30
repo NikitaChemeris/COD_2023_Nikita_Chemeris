d7e654963836be30505f006625321ce3142a63ff
Nikita Chemeris <nikitacemeris9@gmail.com>
2023-03-31 00:04:13 +0300

    Added Task_3, Lection_2

--- Lection_2/Task_3.py
+++ Lection_2/Task_3.py
@@ -1,7 +1,23 @@
 # 3 sets
 
-set1 = set(input('Enter elements for the first set, separated spaces: ').split())
-set2 = set(input('Now for the second set: ').split())
-set3 = set(input('And finally for the third set: ').split())
+list1 = list(input('Enter elements for the first list, separated spaces: ').split())
+list2 = list(input('Now for the second list: ').split())
+list3 = list(input('And finally for the third list: ').split())
 
-print(f'Unique elements from 3 sets: {set1 ^ set2 ^ set3}')
+#No unique from lists
+
+NonUnique1 = [i for i in list1 if list1.count(i) > 1]
+NonUnique2 = [o for o in list2 if list2.count(o) > 1]
+NonUnique3 = [y for y in list3 if list3.count(y) > 1]
+
+print('----------------------------')
+
+print('''No unique elements from lists
+First list: {}
+Second list: {}
+Third list: {}'''.format(NonUnique1, NonUnique2, NonUnique3))
+
+print('----------------------------')
+
+NoUnique = set(NonUnique1) ^ set(NonUnique2) ^ set(NonUnique3)
+print(f'Final result UNIQUE elements from 3 No unique lists: {NoUnique}')
