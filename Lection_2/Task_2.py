eb2d5bef9e914c5b56170f42c6b30106f59e6bd8
Nikita Chemeris <nikitacemeris9@gmail.com>
2023-03-30 23:06:09 +0300

    Added Task_2, Lection_2

--- Lection_2/Task_2.py
+++ Lection_2/Task_2.py
@@ -1,4 +1,4 @@
 # Unique elements
 
-elements = set(input('Enter elements separated be spaces: ').split())
+elements = set(input('Enter elements separated by spaces: ').split())
 print(f'Unique elements - {elements}')
