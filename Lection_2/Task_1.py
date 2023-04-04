d631c719425a1d937dd3b751206a2e5a17d0971e
Nikita Chemeris <nikitacemeris9@gmail.com>
2023-04-04 21:03:50 +0300

    Changed Task_1, Lection_2

--- Lection_2/Task_1.py
+++ Lection_2/Task_1.py
@@ -1,13 +1,13 @@
 '''Counting the Number of Input Elements'''
 
-N = 3
-print(f'Please Enter {N} sentence')
+amount = 3
+print(f'Please Enter {amount} sentence')
 
 elements = []
 
-while N != 0:
+while amount != 0:
     sentence = int(input('Enter count words in sentence: '))
     for i in range(sentence):
         elements.append(input('Enter word: '))
-    N -= 1
+    amount -= 1
 print(f'Amount words = {len(elements)}')
