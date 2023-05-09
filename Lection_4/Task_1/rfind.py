8130a3a6874bdd2ee92f770a52ce56e021940b1d
Nikita Chemeris <nikitacemeris9@gmail.com>
2023-05-09 21:18:10 +0300

    Added Task_1*, Lection_4

--- Lection_4/Task_1/rfind.py
+++ Lection_4/Task_1/rfind.py
@@ -14,7 +14,7 @@ def FindFiles(path, name):
         global_path = os.listdir(path)
     except FileNotFoundError:
         return 'Error: name file not found!'
-    for files in global_path:
+    for files in global_path:  # Recursive method
         if files.find(f'{name}') != -1:
             full_path = os.path.abspath(f'{path}')
             print(f'File: {files}', f'Path: {full_path}', sep='\n')
