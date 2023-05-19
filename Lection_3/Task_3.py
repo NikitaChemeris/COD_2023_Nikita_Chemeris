3a75ad1bf98b4a75c37ccec9d1c3536afa4bde5a
Nikita Chemeris <nikitacemeris9@gmail.com>
2023-05-19 07:25:11 +0300

    Changed Task_3, Lection_3

--- Lection_3/Task_3.py
+++ Lection_3/Task_3.py
@@ -1,9 +1,6 @@
 # Update card file
 
-CardFile = {}
-
-
-def add():
+def add(CardFile, BookId):
     description = {
         'Name': input('Name book: '),
         'Author': input('Author book: '),
@@ -12,42 +9,51 @@ def add():
         'Binding': input('Binding (solid/soft) book: ')
     }
 
+    CardFile[BookId] = description
     print('Book id {} and name book "{}" added'.format(BookId, description['Name']))
-    return description
+    return CardFile, BookId + 1
 
 
-def delete(input_id):
-    print(f'Book id {input_id} and name book "{CardFile[input_id]["Name"]}" deleted')
-    del CardFile[input_id]
-    return input_id
+def delete(CardFile, delete_id):
+    if delete_id in CardFile:
+        print(f'Book id {delete_id} and name book "{CardFile[delete_id]["Name"]}" deleted')
+        del CardFile[delete_id]
+    else:
+        print(f'Book Id {delete_id} not found')
+    return CardFile
 
 
-def edit(change_BookId):
-    category = input("""What do you want to edit? Select and write in Enter:
+def edit(CardFile, edit_id):
+    if edit_id in CardFile:
+        category = input("""What do you want to edit? Select and write in Enter:
 1.Name
 2.Author
 3.Count pages
 4.Genre
 5.Binding
 Please, Enter: """)
-    update = input('Edit on: ')
-
-    CardFile[change_BookId][category] = update
-    print('The {} book id {} replaced by {}'.format(category, change_BookId, update))
+        if category in CardFile[edit_id]:
+            update = input('Edit on: ')
+            CardFile[edit_id][category] = update
+            print('The {} book id {} replaced by {}'.format(category, edit_id, update))
+        else:
+            print(f'Invalid category: {category}')
+    else:
+        print(f'Book Id {edit_id} not found')
     return CardFile
 
 
 def found():
     print(f'Your id and name book : {[(key, value["Name"]) for key, value in CardFile.items()]}')
-
-    info = input('Would you like look info about book? Yes or No: ')
-
+    info = input('Would you like to look up info about book? (Yes/No): ')
     if info == 'Yes':
         input_id = int(input('Please enter id book: '))
-        print(CardFile[input_id])
+        print(CardFile.get(input_id, f'Book Id {input_id} not found'))
 
 
-BookId, function = 0, None
+CardFile = {}
+BookId = 0
+function = None
 
 while function != 'Close':
     function = input('''Select and Write a function for a book:
@@ -60,12 +66,13 @@ Enter: ''')
 
     match function:
         case 'Add book':
-            CardFile[BookId] = add()
-            BookId += 1
+            CardFile, BookId = add(CardFile, BookId)
         case 'Delete book':
-            delete(int(input('Write book id which you want delete: ')))
+            delete_id = (int(input('Write book id which you want delete: ')))
+            CardFile = delete(CardFile, delete_id)
         case 'Edit book':
-            edit(int(input('Write Id book and will edit this: ')))
+            edit_id = (int(input('Write Id book and will edit this: ')))
+            CardFile = edit(CardFile, edit_id)
         case 'Found book':
             found()
         case 'Close':
